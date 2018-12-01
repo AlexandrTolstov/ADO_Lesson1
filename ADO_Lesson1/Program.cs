20406649a27be88a013ce541c4b72a6a21b2dbb7
Толстов Александр <alextav_85@mail.ru>
2018-12-01 09:47:22 +0600

    Add project files.

---
+++ ADO_Lesson1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADO_Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
